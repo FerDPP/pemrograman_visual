618da920d6df088792f9c10e3208fda6ffb3b528
FerDPP <81939136+FerDPP@users.noreply.github.com>
2023-06-23 11:12:58 +0700

    Add files via upload

---
+++ Pert 5 Latihan/Latihan/select case/select case/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Select Case txtkodejabatan.Text
+            Case "MGR" : txtketerangan.Text = "Manager"
+
+            Case "SKR" : txtketerangan.Text = "Sekretaris"
+
+            Case "BDH" : txtketerangan.Text = "Bendahara"
+
+            Case Else : txtketerangan.Text = "Staff"
+        End Select
+    End Sub
+
+    Private Sub txtkodejabatan_TextChanged(sender As Object, e As EventArgs) Handles txtkodejabatan.TextChanged
+
+    End Sub
+End Class
