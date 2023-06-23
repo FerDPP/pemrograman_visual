d81ae0c012fe7d6860863341dbe469541fa6c9ab
FerDPP <81939136+FerDPP@users.noreply.github.com>
2023-06-23 09:40:56 +0700

    Add files via upload

---
+++ Pert 3 Latihan 2/jajar genjang/jajar genjang/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub btnhitung_Click(sender As Object, e As EventArgs) Handles btnhitung.Click
+        Dim a, t, b, luas, keliling As Integer
+
+        a = Val(txtalas.Text)
+        t = Val(txttinggi.Text)
+        b = Val(txtsisimiring.Text)
+
+        luas = a * t
+        keliling = 2 * (a + b)
+
+        txtluas.Text = Str(luas)
+        txtkeliling.Text = Str(keliling)
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label6_Click_1(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+End Class
